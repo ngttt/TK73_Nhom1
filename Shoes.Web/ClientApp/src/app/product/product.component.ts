d48dfedcb4fac4f1d3a5ccfc0c6b5561deaaf920
unknown <1754052021hoa@ou.edu.vn>
2020-06-09 13:28:13 +0700

    angular

---
+++ Shoes.Web/ClientApp/src/app/product/product.component.ts
@@ -0,0 +1,134 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+declare var $:any;
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent implements OnInit {
+   products: any = {
+     data:[],
+     totalRecord: 0,
+     page:0,
+     size:5,
+     TotalPage:0
+   }
+
+   product:any = {
+    productId : 1,
+    productName : "Laptop Asus",
+    categoryId : 1,
+    supplierId : 1,
+    unitPrice : 15000000,
+    quantityPerUnit: "cai"
+  }
+  isEdit: boolean;
+
+  constructor( private http: HttpClient,@Inject('BASE_URL') baseUrl: string) { }
+
+  ngOnInit() {
+    this.searchProduct(1);
+  }
+  searchProduct(cPage){
+    let x = {
+      page : cPage,
+      size :5,
+      keyword:""
+    }
+    this.http.post('https://localhost:44355/api/Productss/search-product',x).subscribe(result=>{
+      this.products = result;
+      this.products = this.products.data;
+      
+    },error=>console.error(error));
+    
+  }
+  searchNext(){
+   
+    if(this.products.page < this.products.totalPage){
+      let nextPage = this.products.page +1;
+      let x = {
+        page : nextPage,
+        size :5,
+        keyword:""
+      }
+      this.http.post('https://localhost:44355/'+'api/Productss/search-product',x).subscribe(result=>{
+        this.products = result;
+        this.products = this.products.data;
+        
+      },error=>console.error(error));
+
+  }
+  else{
+    alert("Bạn đang ở trang cuối cùng");
+  }
+ }
+ searchPrevious(){
+  if(this.products.page>1){
+    let nextPage = this.products.page -1;
+    let x = {
+      page : nextPage,
+      size :5,
+      keyword:""
+    }
+    this.http.post('https://localhost:44355/'+'/api/Productss/search-product',x).subscribe(result=>{
+      this.products = result;
+      this.products = this.products.data;
+      
+    },error=>console.error(error));
+
+}
+else{
+  alert("Bạn đang ở trang đầu tiên");
+}
+}
+openModal(isNew, index) {
+  if(isNew) {
+    this.isEdit = false;
+    this.product = {
+      productId: 0,
+      productName : "",
+      categoryId : 1,
+      supplierId : 1,
+      quantityPerUnit: "",
+      unitPrice : 0
+    }
+  }
+  else {
+    this.isEdit = true;
+    this.product = index;
+  }
+  //mở popup bằng jquery
+  $('#exampleModal').modal("show");
+}
+// Tajo hàm để làm nút add
+addProduct() {
+  var x = this.product;
+  this.http.post('https://localhost:44355/api/Productss/create-product',x).subscribe(result => {
+  var res:any = result;
+  if(res.success) {
+  this.product = res.data;
+  this.isEdit = true;
+  this.searchProduct(1);
+  alert("New products have been added successfully!");
+  }  
+  }, error => console.error(error));
+}
+
+// Tạo hàm làm nút save
+updateProduct() {
+  var x = this.product;
+  this.http.post('https://localhost:44355/api/Productss/update-product',x).subscribe(result => {
+  var res:any = result;
+  if(res.success) {
+  this.product = res.data;
+  this.isEdit = true;
+  this.searchProduct(1);
+  alert("New products have been saved successfully!");
+  }  
+  }, error => console.error(error));
+}
+
+}
+
